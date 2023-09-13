15e12eeeafddede409ba5681c1c84c5a88b2d160
EduardoGarcia935 <eduardo.agarcia117@gmail.com>
2023-09-13 09:42:04 -0300

    feat: começo da solução

--- 8-criacaoDinamica-Arvore/Ex1-ArvorePERGUNTAS.go
+++ 8-criacaoDinamica-Arvore/Ex1-ArvorePERGUNTAS.go
@@ -67,6 +67,66 @@ func somaConcCh(r *Nodo, s chan int) {
 	}
 }
 
+func busca(r* Nodo, v int) bool {
+	if r != nil {
+		if r.v == v {
+			return true
+		}
+		return busca(r.e, v) || busca(r.d, v)
+	}
+	return false
+}
+
+func buscaConc(r* Nodo, v int) bool {
+	s := make(chan bool)
+	go buscaConcCh(r, v, s)
+	return <-s
+}
+func buscaConcCh(r* Nodo, v int, s chan bool) {
+	if r != nil {
+		s1 := make(chan bool)
+		s2 := make(chan bool)
+		go buscaConcCh(r.e, v, s1)
+		go buscaConcCh(r.d, v, s2)
+		s <- (<-s1 || <-s2)
+	} else {
+		s <- false
+	}
+}
+
+func retornaParImpar(r *Nodo, saidaP chan int, saidaI chan int, fin chan struct{}){
+	if r != nil {
+		if r.v % 2 == 0 {
+			saidaP <- r.v
+		} else {
+			saidaI <- r.v
+		}
+		retornaParImpar(r.e, saidaP, saidaI, fin)
+		retornaParImpar(r.d, saidaP, saidaI, fin)
+	} else {
+		fin <- struct{}{}
+	}
+}
+
+func retornaParImparConc(r *Nodo, saidaP chan int, saidaI chan int, fin chan struct{}){
+	if r != nil {
+		s1 := make(chan struct{})
+		s2 := make(chan struct{})
+		go retornaParImparConc(r.e, saidaP, saidaI, s1)
+		go retornaParImparConc(r.d, saidaP, saidaI, s2)
+		<-s1
+		<-s2
+		if r.v % 2 == 0 {
+			saidaP <- r.v
+		} else {
+			saidaI <- r.v
+		}
+	} else {
+		fin <- struct{}{}
+	}
+
+}
+
 // ---------   agora vamos criar a arvore e usar as funcoes acima
 
 func main() {
@@ -95,5 +155,29 @@ func main() {
 	fmt.Println("Soma: ", soma(root))
 	fmt.Println("SomaConc: ", somaConc(root))
 	fmt.Println()
+	fmt.Println("Busca 1: ", busca(root, 12))
+	// DEU MERDA corrige isso
+	//
+
+	fmt.Println("Busca 2 concorrente: ", buscaConc(root, 12))
+
+	fmt.Println()
+	fmt.Println("RetornaParImpar: ")
+	saidaP := make(chan int)
+	saidaI := make(chan int)
+	fin := make(chan struct{})
+	go retornaParImpar(root, saidaP, saidaI, fin)
+	for i := 0; i < 10; i++ {
+		select {
+		case p := <-saidaP:
+			fmt.Println("Par: ", p)
+		case i := <-saidaI:
+			fmt.Println("Impar: ", i)
+		case <-fin:
+			fmt.Println("Fim")
+		}
+	}
+
+
 
 }
